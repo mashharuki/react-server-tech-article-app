ba21a3bc6a5b3ce65afe32a29af52cd90e7c7727
jinwatanabe <jin.watanabe.6g@gmail.com>
2025-02-11 11:42:21 +0900

    スタバで終わった

--- convex/articles.ts
+++ convex/articles.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const get = query({
   args: {},
@@ -43,6 +43,58 @@ export const getPopular = query({
         createdAt: article._creationTime,
         viewCount: article.viewCount,
       };
+    });
+  },
+});
+
+export const insert = mutation({
+  args: {
+    title: v.string(),
+    description: v.string(),
+  },
+  handler: async (ctx, { title, description }) => {
+    await ctx.db.insert("articles", {
+      title,
+      description,
+      author: "@Sicut_study",
+      viewCount: 0,
+    });
+  },
+});
+
+export const getById = query({
+  args: {
+    id: v.id("articles"),
+  },
+  handler: async (ctx, { id }) => {
+    const article = await ctx.db.get(id);
+    if (!article) {
+      throw new Error("Article not found");
+    }
+
+    return {
+      id: article._id,
+      title: article.title,
+      description: article.description,
+      author: article.author,
+      createdAt: article._creationTime,
+      viewCount: article.viewCount,
+    };
+  },
+});
+
+export const incrementViewCount = mutation({
+  args: {
+    id: v.id("articles"),
+  },
+  handler: async (ctx, { id }) => {
+    const article = await ctx.db.get(id);
+    if (!article) {
+      throw new Error("Article not found");
+    }
+
+    await ctx.db.patch(article._id, {
+      viewCount: article.viewCount + 1,
     });
   },
 });
